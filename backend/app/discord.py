c64deab8d6ed9d9737c4ae672cd2a4136d253cae
Sreeraj-004 <sreerajpv2004@gmail.com>
2025-10-16 18:36:01 +0530

    Hide webhook and update Discord URL logic

--- backend/app/discord.py
+++ backend/app/discord.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+DISCORD_WEBHOOK_URL="https://discord.com/api/webhooks/1428241517465829437/wouKhbb0LJrVLiuHkIyACQaUTj5kySoAXi8SSoK63aeJAEJQ_IOXovJL8RJCUaPuMFWM"
 
 def send_discord_notification(contact_data: dict):
     content = f"""
